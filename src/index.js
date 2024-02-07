7df66dda9d137caa9e4276ac74ba0f35c14c194c
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-02-07 12:40:11 +0100

    did exercises until Concert Slot Reservation

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@
 // Welcome and Introduction
 
 // Display a welcoming message for the user.
+alert("Welcome to the Music Festival Concert Slot Reservation! Get ready to choose your favorite music genre!")
+
 
 // Prompt the user to enter their name.
 
+let person = prompt("Please enter your user name")
+
 // Display a greeting message with the user's name.
+
+alert(`Hello, ${person}! Let's get started.`)
 
 // Music Genre Selection
 // Ask the user to choose a music genre.
+
+let musicSelection = prompt(
+    `What is your favorite music genre? 
+    Please enter a number: 
+    1 - Rock 
+    2 - Pop 
+    3 - Electronic`)
 
 // Initialize a variable to store the selected genre.
 
@@ -18,9 +31,25 @@
 
 // Display the chosen genre.
 
+let selectedGenre = ""
+
+if (musicSelection === "1") {
+    selectedGenre = "Rock"
+} else if (musicSelection === "2") {
+    selectedGenre = "Pop"
+} else if (musicSelection === "3") {
+    selectedGenre = "Electronic"
+} else {
+    alert("Invalid choice, please enter 1, 2 or 3")
+}
+
+alert(`You have selected ${selectedGenre} as your favorite genre.`)
+
 // Concert Slot Reservation
 // Ask the user to select a concert slot based on the chosen genre.
 // Determine the available concert slots and corresponding artist names.
+
+
 
 // bonus
 // Determine the selected time slot based on the user's concert slot choice.
