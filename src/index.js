5e3606fcc4df9ae03cb1ca4ed4a9e2196da90d71
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-02-08 12:23:41 +0100

    continued with time slots

--- src/index.js
+++ src/index.js
@@ -18,12 +18,14 @@ alert(`Hello, ${person}! Let's get started.`)
 // Music Genre Selection
 // Ask the user to choose a music genre.
 
-let musicSelection = prompt(
+let musicSelection = parseInt(
+prompt(
     `What is your favorite music genre? 
     Please enter a number: 
     1 - Rock 
     2 - Pop 
     3 - Electronic`)
+    )
 
 // Initialize a variable to store the selected genre.
 
@@ -33,14 +35,15 @@ let musicSelection = prompt(
 
 let selectedGenre = ""
 
-if (musicSelection === "1") {
+if (musicSelection === 1) {
     selectedGenre = "Rock"
-} else if (musicSelection === "2") {
+} else if (musicSelection === 2) {
     selectedGenre = "Pop"
-} else if (musicSelection === "3") {
+} else if (musicSelection === 3) {
     selectedGenre = "Electronic"
 } else {
     alert("Invalid choice, please enter 1, 2 or 3")
+    exit(1)
 }
 
 alert(`You have selected ${selectedGenre} as your favorite genre.`)
@@ -49,7 +52,90 @@ alert(`You have selected ${selectedGenre} as your favorite genre.`)
 // Ask the user to select a concert slot based on the chosen genre.
 // Determine the available concert slots and corresponding artist names.
 
+ let concertSlot = ""
 
+ let artistNames = []
+
+ switch (selectedGenre) {
+    case "Rock":
+        concertSlot = parseInt(
+            prompt(
+                `Select a slot for Rock
+                1 - The Strokes
+                2 - Blink 182
+                3 - Pearl Jam
+                Please enter the number of your choice`
+            )
+        )
+            artistNames = ["The Strokes", "Blink 182", "Pearl Jam"]
+            break
+            
+    case "Pop":
+        concertSlot = parseInt(
+            prompt(
+                `Select a slot for Pop
+                1 - Rihanna
+                2 - Beyoncé
+                3 - Ariana Grande
+                Please enter the number of your choice`
+            )
+        )
+            artistNames = ["Rihanna", "Beyoncé", "Ariana Grande"]
+            break
+
+     case "Electronic":
+        concertSlot = parseInt(
+            prompt(
+                `Select a slot for Pop
+                1 - Tale Impala
+                2 - Björk
+                3 - Daft Punk
+                Please enter the number of your choice`
+            )
+        )
+            artistNames = ["Tale Impala", "Björk", "Daft Punk"]
+            break
+
+        default:
+            alert("Invalid choice, try again")
+ }
+
+    console.log("concert slot", concertSlot)
+
+
+let timeSlot =""
+switch (concertSlot) {
+    case 1: 
+        timeSlot = "18:00"
+        break
+
+    case 2: 
+        timeSlot = "21:00"
+        break
+
+    case 3: 
+        timeSlot = "23:00"
+        break
+
+    default:
+        alert("Invalid choice, try again")
+}
+
+console.log("time slot", timeSlot)
+
+if (concertSlot === 1) {
+    alert (
+        `Great choice! You've reserved a spot to see ${artistNames[0]} at ${timeSlot} for the ${selectedGenre} concert!`
+    )
+} else if (concertSlot === 2) {
+    alert (
+        `Great choice! You've reserved a spot to see ${artistNames[1]} at ${timeSlot} for the ${selectedGenre} concert!`
+    )
+} else if (concertSlot === 3) {
+    alert (
+        `Great choice! You've reserved a spot to see ${artistNames[2]} at ${timeSlot} for the ${selectedGenre} concert!`
+    )
+}
 
 // bonus
 // Determine the selected time slot based on the user's concert slot choice.
